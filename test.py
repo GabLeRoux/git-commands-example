c823a9251580c769d30aff6b78158daaf5cd33ce
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2016-11-21 00:56:36 -0500

    wow such script

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("hello")
+#!/usr/bin/env python
+
+for x in [y**2 for y in range(10)]:
+    if (x < 5):
+        print((x, "x smaller than 5"))
+    else:
+        print((x, "x greater than 5"))
+
